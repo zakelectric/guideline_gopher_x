106ac5cb213cef05ff565e3fbf0228b224b7b676
zakelectric <zmzach@gmail.com>
2024-11-28 12:49:52 -0800

    11.28.24

--- streamlit_analyze.py
+++ streamlit_analyze.py
@@ -5,7 +5,6 @@ import base64
 import time
 import re
 import io
-import PyPDFLoader
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OpenAIEmbeddings
