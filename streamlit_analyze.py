59633ce9acfb2967ea1f2d14772d1c9d864744c3
zakelectric <zmzach@gmail.com>
2024-12-05 15:14:20 -0800

    12.4.24

--- streamlit_analyze.py
+++ streamlit_analyze.py
@@ -217,7 +217,7 @@ class MortgageGuidelinesAnalyzer:
                 try:
                     relevant_chunks = await asyncio.to_thread(self.vector_store.similarity_search, query, k=10)
                     st.write("QUERY:", query)
-                    #st.write("RELEVANT CHUNKS:", relevant_chunks)
+                    st.write("RELEVANT CHUNKS:", relevant_chunks)
                 except Exception as e:
                     st.write("Error with relevant chunks:", e)
 
