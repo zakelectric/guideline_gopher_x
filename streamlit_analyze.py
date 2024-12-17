7aa504185d20e9b9945c60a56779de794e4ff6e8
zakelectric <zmzach@gmail.com>
2024-12-17 11:21:42 -0800

    12.17.24

--- streamlit_analyze.py
+++ streamlit_analyze.py
@@ -144,6 +144,7 @@ class MortgageGuidelinesAnalyzer:
                     )
 
                 analysis = self._parse_llm_response(analysis_response)
+                st.write("ANALYSIS", analysis)
                 
                 if analysis and analysis.get('matches', False):
                     return [{
